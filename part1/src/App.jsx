30e02100c346e46bb4503a8838769b377110e170
EmilianoCBE <e.carrizales1305@gmail.com>
2024-03-18 10:31:52 -0600

    Ejercicios 1.1 y 1.2 hechos

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,24 +1,60 @@
-const Hello = ({name, age}) => {
-  console.log(name, age)
+const Header = ({title}) => {
   return (
     <>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
+      <h1>{title}</h1>
     </>
   )
 }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
+const Part = ({part, exercises}) => {
   return (
     <>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+      <p>{part} {exercises}</p>
     </>
+  )
+}
+
+const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
+  return (
+    <>
+      <Part part={part1} exercises={exercises1}/>
+      <Part part={part2} exercises={exercises2}/>
+      <Part part={part3} exercises={exercises3}/>
+    </>
+  )
+}
+
+const Total = ({total}) => {
+  return (
+    <>
+      <p>Number of exercises {total}</p>
+    </>
+  )
+} 
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const total = exercises1 + exercises2 + exercises3
+
+  return (
+    <div>
+      <Header title={course}/>
+      <Content 
+        part1={part1}
+        part2={part2}
+        part3={part3} 
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total total={total}/>
+    </div>
   )
 }
 
