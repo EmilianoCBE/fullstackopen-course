4d613334fb6153ae40b80c166d56577d47c5a5ba
EmilianoCBE <e.carrizales1305@gmail.com>
2024-03-20 20:53:44 -0600

    Ejercicio 1.9 unicafe paso 4 hecho

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -3,13 +3,21 @@ import { Display } from './Display'
 export const Statistics = ({good, neutral, bad, total, totalAverage, positiveAverage}) => {
   return (
     <>
-      <h1>statistics</h1>
-      <Display text='good' count={good}/>
-      <Display text='neutral' count={neutral}/>
-      <Display text='bad' count={bad}/>
-      <Display text='total' count={total}/>
-      <Display text='average' count={totalAverage ? totalAverage : 0}/>
-      <Display text='positive' count={positiveAverage ? positiveAverage : 0}/>
+      {
+        good||neutral||bad ? 
+          <div>
+            <h1>statistics</h1>
+            <Display text='good' count={good}/>
+            <Display text='neutral' count={neutral}/>
+            <Display text='bad' count={bad}/>
+            <Display text='total' count={total}/>
+            <Display text='average' count={totalAverage ? totalAverage : 0}/>
+            <Display text='positive' count={positiveAverage ? positiveAverage : 0}/>
+          </div>
+          : <div>No feedback given</div>
+        
+      }
+      
     </>
   )
 }
