cc1ff551db71beacf7afddcc0ad41401d2a1a673
EmilianoCBE <e.carrizales1305@gmail.com>
2024-03-24 16:23:23 -0600

    Ejercicio 1.11 unicafe paso 6 ehcho

--- part1/unicafe/src/components/StatisticLine.jsx
+++ part1/unicafe/src/components/StatisticLine.jsx
@@ -1,7 +1,12 @@
 export const StatisticLine = ({text, count}) => {
   return (
-    <div>
-      {text} {count}
-    </div>
+    <table>
+      <tbody>
+        <tr>
+          <td style={{width: '55px'}}>{text}</td>
+          <td>{count}</td>
+        </tr>
+      </tbody>
+    </table>
   )
 }
