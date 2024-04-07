956abd46c561b4b4c42cc3387b91e759e5dddf3c
EmilianoCBE <e.carrizales1305@gmail.com>
2024-04-07 13:24:59 -0600

    Ejerccicio 2.6 La agenda telefónica de Parte 2 hecha

---
+++ part2/agendaTelefonica/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNewNameChange = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const addName = (e) => {
+    e.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: 
+          <input 
+            value={newName}
+            onChange={handleNewNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {
+          persons.map((person) => (
+            <li key={person.name}>
+              {person.name}
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default App
