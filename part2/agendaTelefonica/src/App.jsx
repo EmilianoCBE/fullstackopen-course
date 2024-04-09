364fef201fc725fd69382348938808c204fa9261
EmilianoCBE <e.carrizales1305@gmail.com>
2024-04-08 20:18:52 -0600

    Ejercicio 2.11 de La agenda telefonica hecho de la Parte 2 C

--- part2/agendaTelefonica/src/App.jsx
+++ part2/agendaTelefonica/src/App.jsx
@@ -1,58 +1,61 @@
-import { useState } from 'react'
-import Filter from './components/Filter'
-import { PersonForm } from './components/PersonForm'
-import { Persons } from './components/Persons'
+import { useState, useEffect } from "react";
+import Filter from "./components/Filter";
+import { PersonForm } from "./components/PersonForm";
+import { Persons } from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      const persons = response.data;
+      setPersons(persons);
+    });
+  }, []);
+  
 
   const handleNewNameChange = (e) => {
-    setNewName(e.target.value)
-  }
+    setNewName(e.target.value);
+  };
 
   const handleNewNumberChange = (e) => {
-    setNewNumber(e.target.value)
-  }
+    setNewNumber(e.target.value);
+  };
 
   const handleFilter = (e) => {
-    setFilter(e.target.value)
-  }
+    setFilter(e.target.value);
+  };
 
   const add = (e) => {
-
-    e.preventDefault()
+    e.preventDefault();
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+    };
+
+    if (persons.some((person) => person.name === personObject.name)) {
+      alert(`${newName} is already added to phonebook`);
+      setNewName("");
+      setNewNumber("");
+      return;
     }
 
-    if(persons.some(person => person.name === personObject.name)){
-      alert(`${newName} is already added to phonebook`)
-      setNewName('')
-      setNewNumber('')
-      return
-    }
-
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
-  }
+    setPersons(persons.concat(personObject));
+    setNewName("");
+    setNewNumber("");
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter value={filter} onChange={handleFilter}/>
-      
+      <Filter value={filter} onChange={handleFilter} />
+
       <h2>Add a new contact</h2>
-      <PersonForm 
+      <PersonForm
         onSubmit={add}
         valueName={newName}
         valueNumber={newNumber}
@@ -60,9 +63,9 @@ const App = () => {
         onChangeNumber={handleNewNumberChange}
       />
       <h2>Numbers</h2>
-      <Persons filter={filter} persons={persons}/>
+      <Persons filter={filter} persons={persons} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
