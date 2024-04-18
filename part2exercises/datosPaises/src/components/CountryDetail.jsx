ce3e0f322b26c84aaa13bef274c4fbdf49bdbec0
EmilianoCBE <e.carrizales1305@gmail.com>
2024-04-17 21:09:42 -0600

    Ejercicio 2.20 de Datos de países de la parte 2 e suspendido por API con paywall

--- part2exercises/datosPaises/src/components/CountryDetail.jsx
+++ part2exercises/datosPaises/src/components/CountryDetail.jsx
@@ -1,13 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const CountryDetail = ({detail}) => {
+  const apiKey = import.meta.env.VITE_WEATHER_KEY;
+  const [weather, setWeather] = useState(null)
+
   const languages = Object.values(detail.languages)
   const flagSrc = detail.flags.png.toString()
   const flagAlt = detail.flags.alt.toString()
+
+  const lat = detail.capitalInfo.latlng[0]
+  const lon = detail.capitalInfo.latlng[1]
+
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+      .then(response =>
+        console.log(response)  
+      )
+  },[])
 
   return (
     <>
       <h2>{detail.name.common}</h2>
       <p>{detail.capital[0]}</p>
-      <p>{detail.area}</p>
+      <p>Area {detail.area}</p>
       <h3>Languages:</h3>
       <ul>
         {
