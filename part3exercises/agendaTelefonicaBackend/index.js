22e4134399712d3c1792214954b9a0bb48a029db
EmilianoCBE <e.carrizales1305@gmail.com>
2024-04-22 20:15:52 -0600

    Ejericio 3.5 de la Agenda telefónica de la Parte 3 hecho

--- part3exercises/agendaTelefonicaBackend/index.js
+++ part3exercises/agendaTelefonicaBackend/index.js
@@ -56,6 +56,31 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+const generateId = () => {
+  const newId = Math.floor(Math.random() * 100)
+  return newId
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
